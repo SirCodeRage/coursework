33290dd7be6b680dd54e92ae2407da162b27ecc4
SirCodeRage <148248482+SirCodeRage@users.noreply.github.com>
2023-10-25 15:24:18 +0100

    Update python_basic connect.py

--- python_basic connect.py
+++ python_basic connect.py
@@ -4,8 +4,8 @@ from netmiko import ConnectHandler
 iosv_l2 = {
     'device_type' : 'cisco_ios',
     'ip' : '192.168.56.101',
-    'username' : 'user5',
-    'password' : 'cisco'
+    'username' : 'prne',
+    'password' : 'cisco123'
 }
 net_connect = ConnectHandler(**iosv_l2)
 output = net_connect.send_command("show ip int brief")#
