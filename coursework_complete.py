16c7bc30e1782246d7bda67b7013ecc3da13e323
SirCodeRage <148248482+SirCodeRage@users.noreply.github.com>
2023-10-25 16:00:36 +0100

    Update coursework_complete.py
    updated commands

--- coursework_complete.py
+++ coursework_complete.py
@@ -24,10 +24,10 @@ def telenet():
     output = net_connect.send_command("show ip int brief")#
     print(output)
 
-option = input("which way would you like to access the router:\nPress 1 for telenet or press 2 for SSH:\n")
-if option == 1:
+option = input(int("which way would you like to access the router:\nPress 1 for telenet or press 2 for SSH:\n"))
+if option == "1":
     ssh()
-elif option ==2:
+elif option == "2":
     telenet()
 else:
     print("please press 1 or 2")
