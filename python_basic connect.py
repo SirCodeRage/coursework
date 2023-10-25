a74dcd03be76f95353a1658718874167d17b9df5
SirCodeRage <148248482+SirCodeRage@users.noreply.github.com>
2023-10-25 15:33:01 +0100

    Update python_basic connect.py

--- python_basic connect.py
+++ python_basic connect.py
@@ -6,7 +6,7 @@ iosv_l2 = {
     'device_type' : 'cisco_ios',
     'ip' : '192.168.56.101',
     'username' : 'prne',
-    'password' : 'cisco123!'
+    'password' : password,
 }
 net_connect = ConnectHandler(**iosv_l2)
 output = net_connect.send_command("show ip int brief")#
