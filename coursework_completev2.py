782bb5429fddae681003b1450986a02ab64d5e9e
SirCodeRage <148248482+SirCodeRage@users.noreply.github.com>
2023-11-02 23:50:12 +0000

    Update coursework_completev2.py

--- coursework_completev2.py
+++ coursework_completev2.py
@@ -1,18 +1,20 @@
-import netmiko
+#import the libraries that are used 
+import netmiko 
 import getpass
-def ssh():
+def ssh(): # defines the function for ssh router 
     from netmiko import ConnectHandler
-    iosv_l2 = {
+    iosv_l2 = { # define the parameter to connect to the router 
     'device_type' : 'cisco_ios',
-    'ip' : '192.168.56.101',
-    'username' : 'prne',
+    'ip' : '192.168.56.101', #ip address 
+    'username' : 'cisco', # routers username 
     'password' : ''
     }
-    iosv_l2['password'] = getpass.getpass("Enter password:")
-    net_connect = ConnectHandler(**iosv_l2)
-    command = ["exit","show ip int brief",'enable','config t','hostname R1']
+    iosv_l2['password'] = getpass.getpass("Enter password:") # hides the password when typed and don't store it in varible 
+    
+    net_connect = ConnectHandler(**iosv_l2)    command = ["exit","show ip int brief",'enable','config t','hostname R1']#  set of commands that are given to the router. it starts of in config t
+    
     config = net_connect.send_config_set(command)
-    print(config)
+    print(config) # print the 
     file = open("config_setup_ssh.txt", "w")
     file.write(config)
 def telenet():
